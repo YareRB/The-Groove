a07ab1ef54b8f6cee822cbe1f53499e0ccb10c76
YareliRB <yareram78@gmail.com>
2022-06-15 22:29:34 -0500

    Cambios php

---
+++ the-groove/pages/orderdetail/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Order</title>
+    <link rel = "icon" href = "./icons/detail-order.svg" type = "image/x-icon">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/orders.css">
+    <link rel="stylesheet" href="./css/cart.css">
+    <link rel="stylesheet" href="./css/orderdetail.css">
+    <link rel="stylesheet" href="./css/index.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Lato:wght@900&display=swap" rel="stylesheet">
+
+</head>
+<body class="black-scroll">
+    <div class="container-fluid margin-t-10 ps-5 pe-5">
+        <div class="row  ms-5">
+            <div class="col-12 col-sm-12 col-md-7 col-lg-6 col-xl-6 col-xxl-6 parent">
+                <img class="img-disc-cart" src="./images/vinyl.svg" alt="">
+                <img class="img-cart" src="./images/thank-u-next-ariana-grande.svg" alt="">
+            </div>
+            <div class="col-11 col-sm-11 col-md-4 col-lg-5 col-xl-5 col-xxl-5 m-t-100 m-s-20 ">
+                <h3 class="title-lato">Ariana Grande - thank u, next</h3>
+                <label class="text"> <strong>No. Order:</strong> <label class="ms-3">1</label></label>
+                <br>
+                <label class="text"> <strong>Total  :</strong> <label class="ms-5">$2400</label></label>
+
+                <h5 class="pt-1 color-gray text mt-2"> Arrived </h5>
+                <h4 class="pt-1 title-inter-25 pt-2"> Price: $800</h4>
+            </div>
+        </div>
+
+        <label class="text ms-5 ps-4 mt-5"> <strong>Bought with:</strong></label>
+
+        <div class="row mt-3">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 parent">
+                <img class="img-disc-cart vinyl" src="./images/vinyl.svg" alt="">
+                <img class="img-cart disc" src="./images/after-hours-the-weeknd.svg" alt="">
+                <h5 class="color-gray text mt-3 ms-5 ps-4">The weeknd, after hours </h5>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 parent">
+                <img class="img-disc-cart vinyl" src="./images/vinyl.svg" alt="">
+                <img class="img-cart disc" src="./images/after-hours-the-weeknd.svg" alt="">
+                <h5 class="color-gray text mt-3 ms-5 ps-4">The weeknd, after hours </h5>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 parent">
+                <img class="img-disc-cart vinyl" src="./images/vinyl.svg" alt="">
+                <img class="img-cart disc" src="./images/after-hours-the-weeknd.svg" alt="">
+                <h5 class="color-gray text mt-3 ms-5 ps-4">The weeknd, after hours </h5>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 parent">
+                <img class="img-disc-cart vinyl" src="./images/vinyl.svg" alt="">
+                <img class="img-cart disc" src="./images/after-hours-the-weeknd.svg" alt="">
+                <h5 class="color-gray text mt-3 ms-5 ps-4">The weeknd, after hours </h5>
+            </div>
+
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- the-groove/pages/payment/index.php
+++ the-groove/pages/payment/index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment</title>
-    <link rel = "icon" href = "/icons/credit-card.svg" type = "image/x-icon">
+    <link rel = "icon" href = "./icons/credit-card.svg" type = "image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/payment.css">
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="./css/payment.css">
+    <link rel="stylesheet" href="./css/index.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,41 +24,41 @@
             <div class="pt-2 ps-3 bg-gray pb-2">
                 <div class="form-check">
                     <input class="form-check-input bg-black" type="radio" name="oxxo" id="oxxo" value="oxxo"/>
-                    <label class="form-check-label general-text ms-2" for="oxxo"> OXXO </label>
-                    <img class="img-icon" src="/icons/oxxo.svg" alt="OXXO">
+                    <label class="form-check-label text ms-2" for="oxxo"> OXXO </label>
+                    <img class="img-icon" src="./icons/oxxo.svg" alt="OXXO">
                 </div>
                 <div class="mt-2 ps-5 mb-4" id="oxxo-info" hidden="true">
-                    <label class="general-text ">REFERENCE</label>
+                    <label class="text ">REFERENCE</label>
                     <br>
-                    <input class="input-r general-text ps-4" type="text" readonly placeholder="35481102556">
+                    <input class="input-r text ps-4" type="text" readonly placeholder="35481102556">
                 </div>
             </div>
             <!-- CREDIT CARD -->
             <div class="mt-3 pt-2 ps-3 bg-gray pb-2">
                 <div class="form-check">
                     <input class="form-check-input bg-black" type="radio" name="card" id="card" value="card"  />
-                    <label class="form-check-label general-text  ms-2" for="credit-card"> Credit Card</label>
-                    <img class="img-icon" src="/icons/visa.svg" alt="VISA">
-                    <img class="img-icon" src="/icons/mastercard.svg" alt="mastercard">
+                    <label class="form-check-label text  ms-2" for="credit-card"> Credit Card</label>
+                    <img class="img-icon" src="./icons/visa.svg" alt="VISA">
+                    <img class="img-icon" src="./icons/mastercard.svg" alt="mastercard">
 
                 </div>
                 <div class="mt-2 ps-5 row mb-4" id="credit-info" hidden="false">
                     <div class="col-12 col-sm-12 col-md-9 col-lg-8 col-xl-8 col-xxl-8">
                         <form action="">
                             <label class="title-inter-12 mt-2" for="">Cardholder name:</label><br>
-                            <input class="input-r general-text ps-4 w-75" type="text" placeholder="Cardholder name" required>
+                            <input class="input-r text ps-4 w-75" type="text" placeholder="Cardholder name" required>
                             <br>
                             <label class="title-inter-12 mt-2" for="">Card number:</label><br>
-                            <input class="input-r general-text ps-4 w-75" type="text" minlength="16" maxlength="16" placeholder="card number" required>
+                            <input class="input-r text ps-4 w-75" type="text" minlength="16" maxlength="16" placeholder="card number" required>
                             <br>
                             <label class="title-inter-12 mt-2" for="">Expiration date</label><label class="title-inter-12 mt-2 ms-25" for="">Security code:</label><br>
-                            <input class="input-r general-text ps-4 w-50 " type="month"  placeholder="expiration date" required>
+                            <input class="input-r text ps-4 w-50 " type="month"  placeholder="expiration date" required>
 
-                            <input class="input-r general-text ps-4 w-25" type="text" minlength="3" maxlength="3" placeholder="CVV" required>
+                            <input class="input-r text ps-4 w-25" type="text" minlength="3" maxlength="3" placeholder="CVV" required>
                         </form>
                     </div>
                     <div class="col-12 col-sm-12 col-md-3 col-lg-4 col-xl-4 col-xxl-4">
-                        <img src="/icons/card.svg" alt="" class="w-100 pe-5">
+                        <img src="./icons/card.svg" alt="" class="w-100 pe-5">
                     </div>
                    
                 </div>
@@ -67,7 +67,7 @@
 
             <!-- MAPA -->
             <div class="mt-3 pt-2 ps-3 pb-2 w-100">
-                <img class="w-100" src="/images/happier-than-ever-billie-eilish.svg" alt="">
+                <img class="w-100" src="./images/happier-than-ever-billie-eilish.svg" alt="">
             </div>
         </div>
         <!-- INFO PAGO -->
@@ -75,13 +75,15 @@
             <div class="text-center">
                 <h5 class="title-inter bg-dark text-white mt-2 pb-1 pt-1"> Order summary</h5>
             </div>
-            <label class="general-text ms-2 mt-2"> <strong>No. Order:</strong> <label class="ms-3">1</label></label>
+            <label class="text ms-2 mt-2"> <strong>No. Order:</strong> <label class="ms-3">1</label></label>
             <br>
-            <label class="general-text ms-2"> <strong>Subtotal:</strong> <label class="ms-4">$2400</label></label>
+            <label class="text ms-2"> <strong>Subtotal:</strong> <label class="ms-4">$2400</label></label>
             <br>
-            <label class="general-text ms-2"> <strong>Envio:</strong> <label class="ms-5">$400</label></label><br>
-            <label class="general-text ms-2 mt-4"> <strong>Total to pay: <label for="">$2800</label></strong></label>
-            <button type="button" class="btn bg-dark radius-l radius-r text-white title-inter-12 btnFinish " >FINISH</button>
+            <label class="text ms-2"> <strong>Envio:</strong> <label class="ms-5">$400</label></label><br>
+            <label class="text ms-2 mt-4"> <strong>Total to pay: <label for="">$2800</label></strong></label>
+            <a href="?seccion=products"> 
+                <button type="button" class="btn bg-dark radius-l radius-r text-white title-inter-12 btnFinish " >FINISH</button>
+            </a>
 
         </div>
     </div>
