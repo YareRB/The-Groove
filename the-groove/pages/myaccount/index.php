18ae7e026475c8c7f12c99af0a2e39fdee56ff91
Yanet Villa Rojo <yanetrojo01@gmail.com>
2022-06-19 04:23:21 -0500

    Avance MyOrders en MyAccount

--- the-groove/pages/myaccount/index.php
+++ the-groove/pages/myaccount/index.php
@@ -38,38 +38,78 @@
     <div class="orders">
         <p class="subtitle2">My Orders</p>
         <div class="row">
+
             <div class="col-lg-3 col-md-6 col-sm-12">
                 <img width="250px" height="250px" class="img-fluid" src="./images/after-hours-the-weeknd.svg" alt="">
                 <p class="general-text">Title - artist</p>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/blonde-frank-ocean.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/blood-type-cautious-clay.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/ctrl-sza.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/dawn-sg-lewis.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/harrys-house-harry-styles.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/happier-than-ever-billie-eilish.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img width="250px" height="250px" class="img-fluid" src="./images/golden-hour-kacey-musgraves.svg" alt="">
-                <p class="general-text">Title - artist</p>
-            </div>
+
+            <?php 
+                $id = $_SESSION["id"];
+
+                $consulta  = "SELECT idOrder FROM jramirez.order
+                              WHERE idUser = ?;";
+
+                $query = $conn -> prepare($consulta);
+                $query -> bindParam(1, $id);
+                $query -> execute();
+
+                while($orderitem = $query -> fetch()) {
+
+                    $idOrder = $orderitem["idOrder"];
+
+                    $consultaDetail  = "SELECT idVinyl FROM jramirez.detailOrder
+                                WHERE idOrder = ?;";
+
+                    $queryDetail = $conn -> prepare($consultaDetail);
+                    $queryDetail -> bindParam(1, $idOrder);
+                    $queryDetail -> execute();
+
+                    $total = 0;
+                    $total = $queryDetail -> rowCount();
+
+                    if ($total > 0) {
+
+                        while($orderdetailitem = $queryDetail -> fetch()) {
+
+                            $idVinyl = $orderdetailitem["idVinyl"];
+        
+                            $consultaVinyl  = "SELECT * FROM vinyl
+                                        WHERE idVinyl = ?;";
+        
+                            $queryVinyl = $conn -> prepare($consultaVinyl);
+                            $queryVinyl -> bindParam(1, $idVinyl);
+                            $queryVinyl -> execute();
+                            
+                            while($vinyl = $queryVinyl -> fetch()) {
+
+                                //Titulo e imagen
+                                print($vinyl["image"]);
+                                print($vinyl["vinylName"]);
+
+                                $idArtist = $vinyl["idArtist"];
+
+                                $consultaArtist  = "SELECT * FROM artist
+                                        WHERE idArtist = ?;";
+        
+                                $queryArtist = $conn -> prepare($consultaArtist);
+                                $queryArtist -> bindParam(1, $idArtist);
+                                $queryArtist -> execute();
+
+                                while($artist = $queryArtist -> fetch()) {
+
+                                    //Artista
+                                    print($artist["artistName"]);
+
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            ?>
+
         </div>
     </div>
     </div>    
